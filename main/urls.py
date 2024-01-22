859ec2928adaafd5e72e88a281adcb834225b26b
Kamalh27 <plnkamalhosen@gmail.com>
2024-01-22 14:55:12 +0700

    update static path

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import api
 
 urlpatterns = [
-    path('/', api.api),
+    path('api/', api.api),
 ]
--- medash/settings.py
+++ medash/settings.py
@@ -85,6 +85,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 STATICFILES_DIRS = [
